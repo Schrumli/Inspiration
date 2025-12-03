8344a3d4b3f15acc1665ca90f1cd319f51fe723d
Cedric Keller <cedrkeller@ethz.ch>
2025-12-03 11:10:47 +0900

    Make long lists scrollable

--- app/src/main/java/com/schrumli/inspiration/MainActivity.kt
+++ app/src/main/java/com/schrumli/inspiration/MainActivity.kt
@@ -10,9 +10,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Delete
@@ -309,7 +312,12 @@ fun ShowList(list: ActivityList, closeList: () -> Unit) {
 
         if (showList) {
             Spacer(Modifier.height(8.dp))
-            Column {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .size(400.dp)
+                    .verticalScroll(rememberScrollState())
+            ){
                 if (list.size() > 0) {
                     for (item in list.all()) {
                         Text(text = item, style = MaterialTheme.typography.bodyLarge)
